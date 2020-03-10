25e1f09411e842f28713e5b01b1cbc46bfafcbcb
Adam Rosien <adam.rosien@underscore.io>
2020-03-10 12:00:53 -0700

    include png in slides build

--- bin/slides.sh
+++ bin/slides.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 asciidoctor-revealjs -r asciidoctor-diagram -D target/asciidoctor -a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@3.9.2 src/main/asciidoctor/slides.adoc 
-cp src/main/asciidoctor/*.jpg src/main/asciidoctor/*.css target/asciidoctor
+cp src/main/asciidoctor/*.jpg src/main/asciidoctor/*.png src/main/asciidoctor/*.css target/asciidoctor
